08ac9b198c6edbad2059f386ffc0be097166b4fe
Ju/TroisYaourts <julien@troisyaourts.com>
2020-10-02 17:04:39 +0200

    rotate + robot name + const fix

--- src/Position.php
+++ src/Position.php
@@ -12,7 +12,7 @@ class Position{
   const WEST = "W";
 
   const LEFT = "L";
-  const RIGHT = "L";
+  const RIGHT = "R";
 
   public function __construct($x, $y, $direction = self::NORTH){
     $this->x = $x;
