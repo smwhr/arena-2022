63433215c9faf699e39b9f70afc3a2aa4b98dd81
Olyno <olyno.dev@gmail.com>
2020-10-02 16:17:09 +0200

    :sparkles: Added Bulma css

--- templates/header.html.php
+++ templates/header.html.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>arena-2021</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css">
 </head>
 <body>
     
