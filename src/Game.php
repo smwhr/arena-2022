552c384ab85b173b2118aa232902cd3904b8413a
Aikru <urkia.p@free.fr>
2020-10-02 16:05:31 +0200

    Add a watcher who count survivor each turn

--- src/Game.php
+++ src/Game.php
@@ -1,44 +1,47 @@
 <?php
+
 namespace RobotWar;
 
-class Game{
+class Game
+{
 
   private $lifeManager;
   private $positionManager;
   private $robots;
 
   public function __construct(
-      $robots, $arena
-  ){
+    $robots,
+    $arena
+  ) {
 
     $positionManager = new Manager\PositionManager($arena);
-    foreach($robots as $r){
+    foreach ($robots as $r) {
       $positionManager->addRobot($r);
     }
 
     $lifeManager = new Manager\LifeManager();
-    foreach($robots as $r){
+    foreach ($robots as $r) {
       $lifeManager->addRobot($r);
     }
 
     $this->lifeManager = $lifeManager;
     $this->positionManager = $positionManager;
     $this->robots = array_combine(
-            ["A", "B"], 
-            $robots
-          );
-
+      ["A", "B"],
+      $robots
+    );
   }
 
-  public function nextTurn(){
+  public function nextTurn()
+  {
     // @todo
     /*
        1. transmettre à chaque robot
           les infos 
     */
-    foreach($this->robots as $letter => $robot){
-        $surroundings = $this->positionManager->getSurroundings($letter);
-        $robot->setSurroundings($surroundings);
+    foreach ($this->robots as $letter => $robot) {
+      $surroundings = $this->positionManager->getSurroundings($letter);
+      $robot->setSurroundings($surroundings);
     }
 
     /*
@@ -50,14 +53,14 @@ class Game{
               - tirer
         */
     $actions = [];
-    foreach($this->robots as $letter => $robot){
+    foreach ($this->robots as $letter => $robot) {
       $actions[$letter] = $robot->requestAction();
     }
 
     /*
         3. verifier que c'est possible
     */
-    $actions = array_filter($actions, function($letter, $action){
+    $actions = array_filter($actions, function ($letter, $action) {
       return $this->can($letter, $action);
     }, ARRAY_FILTER_USE_BOTH);
 
@@ -65,18 +68,19 @@ class Game{
         4. effectuer
     */
     $report = [];
-    foreach($actions as $letter => $action){
-        $report[] = $this->do($letter, $action);
+    foreach ($actions as $letter => $action) {
+      $report[] = $this->do($letter, $action);
     }
 
     /*
         5. vérifier les conditions de victoire
     */
-        $survivors = $this->lifeManager->getSurvivors();
-        if(count($survivors) <= 1){
-          throw new WinningCondition($survivors);
-        }
 
-        return $report;
+
+    if (count($survivors) <= 1) {
+      throw new WinningCondition($survivors);
+    }
+
+    return $report;
   }
 }
--- src/Manager/LifeManager.php
+++ src/Manager/LifeManager.php
@@ -1,24 +1,50 @@
 <?php
+
 namespace RobotWar\Manager;
 
 use RobotWar\Robot;
 
-class LifeManager{
-  
+class LifeManager
+{
+
   private $spawnPosition;
   private $lives;
 
-  public function __construct(){
-      $this->spawnPosition = "A";
-      $this->lives = [];
+  public function __construct()
+  {
+    $this->spawnPosition = "A";
+    $this->lives = [];
   }
 
-  public function addRobot(Robot $robot){
+  public function get_life()
+  {
+    return $this->lives;
+  }
 
-      $this->lives[$this->spawnPosition] = 10;
+  public function get_SpawnLetter()
+  {
+    return $this->spawnPosition;
+  }
 
-      if($this->spawnPosition == "A"){
-        $this->spawnPosition = "B";
+
+  public function CountSurvivor()
+  {
+    $survivor = strlen(get_life());
+    $robotlife = $this->lifeManager->get_life();
+    foreach ($robotlife as $value)
+      if ($value = 0) {
+         $survivor = $survivor- 1
       }
+      return $survivor
+  }
+
+  public function addRobot(Robot $robot)
+  {
+
+    $this->lives[$this->spawnPosition] = 10;
+
+    if ($this->spawnPosition == "A") {
+      $this->spawnPosition = "B";
+    }
   }
 }
