a15c8bd69dba6690428363ecfbd75e187d5c8698
Noémie <noemie.kpatenon@supinternet.fr>
2020-10-15 13:02:13 +0200

    Update life.html.php

--- templates/life.html.php
+++ templates/life.html.php
@@ -1,29 +1,3 @@
 <?php
 include("../templates/header.html.php");
-include("../Manager/LifeManager.php");
-
-use Manager\LifeManager;
-
-use RobotWar\Robot;
-
-class LifeManager{
-  
-  private $spawnPosition;
-  private $lives;
-
-  public function __construct(){
-      $this->spawnPosition = "A";
-      $this->lives = [];
-  }
-
-  public function addRobot(Robot $robot){
-
-      $this->lives[$this->spawnPosition] = 10;
-
-      if($this->spawnPosition == "A"){
-        $this->spawnPosition = "B";
-      }
-  }
-}
-
-?>
+include("../Manager/LifeManager.php");?>
