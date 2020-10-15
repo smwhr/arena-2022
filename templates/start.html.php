65d83f453c9bd63106bd7d223cf9a75a7f01f119
Olyno <olyno.dev@gmail.com>
2020-10-15 15:38:06 +0200

    :sparkles: Added start page data

--- templates/start.html.php
+++ templates/start.html.php
@@ -1,8 +1,14 @@
 <div class="columns is-centered is-vcentered has-text-centered">
   <div class="column">
     <h1 class="title">Partie initialisée</h1>
-    <br>
-    @todo : recap
+    <section class="section">
+      <?php 
+        $game = $_SESSION['game'];
+        foreach($game -> getRobotStats() as $robot) {
+          echo '<p>' . $robot[0] . ' est en train de jouer avec ' . $robot[1] . ' points de vie</p>';
+        }
+      ?>
+    </section>
     <form action="/next">
       <button class="button is-info">C'est parti</button>
     </form>
